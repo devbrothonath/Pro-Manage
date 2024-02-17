246e56e4f7fbf81f913c647315774a7ca47ac0e6
Devbrotho Nath <devbrotho.dn@gmail.com>
2024-02-17 19:42:46 +0530

    fetching data from api

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Home = () => {
+    const [tasks, setTasks] = useState(null)
+
+    useEffect(() => {
+        const fetchTasks = async () => {
+            const response = await fetch("http://localhost:5000/api/tasks")
+            const json = await response.json()
+
+            if(response.ok) {
+                setTasks(json)
+            }
+        }
+
+        fetchTasks()
+    }, [])
   return (
-    <div>Home</div>
+    <div className='home'>
+        <div className="tasks">
+            {tasks && tasks.map((task) => (
+                <p key={task._id}>{task.title}</p>
+            ))}
+        </div>
+    </div>
   )
 }
 
