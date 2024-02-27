3a47247cefa857a85f85badea1b897983541a884
Devbrotho Nath <devbrotho.dn@gmail.com>
2024-02-27 10:29:52 +0530

    auth cloud add

--- Frontend/src/hooks/useLogin.jsx
+++ Frontend/src/hooks/useLogin.jsx
@@ -10,7 +10,7 @@ const useLogin = () => {
     setIsLoading(true);
     setError(null);
 
-    const response = await fetch("http://localhost:5000/api/user/login", {
+    const response = await fetch("https://pro-manage-xv2j.onrender.com/api/user/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
--- Frontend/src/hooks/useRegister.jsx
+++ Frontend/src/hooks/useRegister.jsx
@@ -10,7 +10,7 @@ const useRegister = () => {
     setIsLoading(true);
     setError(null);
 
-    const response = await fetch("http://localhost:5000/api/user/register", {
+    const response = await fetch("https://pro-manage-xv2j.onrender.com/api/user/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ name, email, password, confirmPassword }),
